3f44e6f68dbbd2050a3e2815ee00ce8752967ee1
BitterKass <ariacasellas@inspladelestany.cat>
2022-09-16 17:00:18 +0200

    posant un comentari

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 class UnitatFormativa:
     nom = None
 
+
     def __init__(self, nom):
         self.nom = nom
 
@@ -16,14 +17,3 @@ uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes")
 print(uf1.nom)
 print(uf2.nom)
 print(uf3.nom)
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
