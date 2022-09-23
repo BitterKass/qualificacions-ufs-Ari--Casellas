75147a1fcdf2b9711b65ab8eba7dc8d2ac5bb67d
BitterKass <ariacasellas@inspladelestany.cat>
2022-09-23 17:04:39 +0200

    penjant codi actualitzat

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from unitat_formativa import UnitatFormativa
 
 
 def inicialitzar_modul_professional():
-    global uf1, uf2, uf3, uf1
-    # Inici del programa
+    global uf1, uf2, uf3, mp5
     uf1 = UnitatFormativa("UF1. Desenvolupament del programari", 20)
     uf2 = UnitatFormativa("UF2. Optimització del programari", 20)
     uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes", 26)
@@ -13,19 +12,18 @@ def inicialitzar_modul_professional():
     uf2.qualificacio = 10
     uf3.qualificacio = 4
     mp5 = ModulProfessional("MP05. Entorns de desenvolupament")
-    mp5.afegir_unitatformativa(uf1)
-    mp5.afegir_unitatformativa(uf2)
-    mp5.afegir_unitatformativa(uf3)
+    mp5.afegir_unitat_formativa(uf1)
+    mp5.afegir_unitat_formativa(uf2)
+    mp5.afegir_unitat_formativa(uf3)
 
 
-inicialitzar_modul_professional()
-
-
-def mostra_modul_professional():
+def mostrar_dades_modul_professional():
     print(uf1.nom, ":", uf1.qualificacio)
     print(uf2.nom, ":", uf2.qualificacio)
     print(uf3.nom, ":", uf3.qualificacio)
     print(mp5.nom, ":", mp5.get_qualificacio())
 
 
-mostra_modul_professional()
+# Inici del programa
+inicialitzar_modul_professional()
+mostrar_dades_modul_professional()
